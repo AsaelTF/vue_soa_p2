18a908d29b511ab289953ec4b71be2abc1e23207
Asael Tun Franco <asael_tunfranco@hotmail.com>
2023-06-21 15:20:54 -0500

    add empleados view

--- src/views/EmpleadosView.vue
+++ src/views/EmpleadosView.vue
@@ -2,10 +2,25 @@
   <p class="text-2xl text-center font-Roboto font-bold tracking-wide my-5">
     Empleados
   </p>
+  <div class="flex justify-end w-full mb-5">
+    <div class="flex flex-col w-64 gap-y-3">
+      <input
+        class="border focus:no-underline ring-0"
+        type="text"
+        name=""
+        id=""
+        v-model="correo"
+      />
+      <button
+        @click="correoMasivo()"
+        class="text-sm font-medium bg-indigo-700 text-white rounded"
+      >
+        Enviar correos masivo a todos los empleados
+      </button>
+    </div>
+  </div>
   <div class="min-h-screen">
-    <CardPersona
-	:arrayEmpleados="arrayEmpleados"
-	></CardPersona>
+    <CardPersona :arrayEmpleados="arrayEmpleados"></CardPersona>
   </div>
 </template>
 
@@ -16,6 +31,7 @@ import CardPersona from "../components/CardPersona.vue";
 export default {
   data() {
     return {
+      correo:"",
       arrayEmpleados: null,
       xmlData: `<?xml version="1.0" encoding="utf-8"?>
 <soap:Envelope xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/">
@@ -52,7 +68,6 @@ export default {
 
           console.log(this.arrayEmpleados);
         } else {
-
           console.log("No se encontró la cadena JSON en la respuesta.");
         }
       })
@@ -60,6 +75,34 @@ export default {
         console.log(error);
       })
       .finally(() => {});
+  },
+  methods: {
+    correoMasivo() {
+      xmlCorreo: `<?xml version="1.0" encoding="utf-8"?>
+<soap:Envelope xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/">
+  <soap:Body>
+    <EnviarCorreos xmlns="http://tempuri.org/">
+      <mensaje>${this.correo}</mensaje>
+    </EnviarCorreos>
+  </soap:Body>
+</soap:Envelope>
+`,
+axios
+      .post("https://soa-wcf.azurewebsites.net/Service.svc", this.xmlData, {
+        headers: {
+          "Content-Type": "text/xml; charset=utf-8",
+          SOAPAction: "http://tempuri.org/IService/EnviarCorreos",
+        },
+        maxContentLength: Infinity,
+      })
+      .then((response) => {
+
+      })
+      .catch((error) => {
+        console.log(error);
+      })
+      .finally(() => {});
+    },
   },
   components: {
     CardPersona,
